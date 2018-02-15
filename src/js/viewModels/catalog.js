09924d653f5bfd577ba00a971bca19ed4e4a48d3
Lucas Jellema <lucasjellema@gmail.com>
2018-02-15 18:07:18 +0100

    Support for communication to and from embedding application (through IFRAME events)

--- src/js/viewModels/catalog.js
+++ src/js/viewModels/catalog.js
@@ -6,24 +6,57 @@ define(
         function CatalogModel() {
             var self = this;
 
-            var data = [{ name: 'Stroopwafels', version: '10.3.6', nodes: 2, cpu: 2, type: 'Java Cloud Service Virtual Image', balancer: 1, memory: 8 },
+            self.productData = [{ name: 'Stroopwafels', version: '10.3.6', nodes: 2, cpu: 2, type: 'Java Cloud Service Virtual Image', balancer: 1, memory: 8 },
             { name: 'Klompen', version: '10.3.6', nodes: 2, cpu: 2, type: 'Java Cloud Service Virtual Image', balancer: 1, memory: 8 },
             { name: 'Mini-Molentjes', version: '10.3.6', nodes: 2, cpu: 2, type: 'Java Cloud Service Virtual Image', balancer: 1, memory: 8 },
             { name: 'Delfts Blauw Spul', version: '10.3.6', nodes: 2, cpu: 2, type: 'Java Cloud Service Virtual Image', balancer: 1, memory: 8 },
             { name: 'Drop', version: '10.3.6', nodes: 2, cpu: 2, type: 'Java Cloud Service Virtual Image', balancer: 1, memory: 8 }
             ];
-            self.dataProvider = new oj.ArrayDataProvider(data,
+            self.dataProvider = new oj.ArrayDataProvider(self.productData,
                 {
-                    keys: data.map(function (value) {
+                    keys: self.productData.map(function (value) {
                         return value.name;
                     })
                 });
 
+            // this function will communicate an event with the parent window
+            // typically used for applications that run inside an IFRAME to inform the
+            // embedding application about what is going on.      
+            self.callParent = function (message) {
+                console.log('send message from Catalog to parent window');
+                // here we can restrict which parent page can receive our message
+                // by specifying the origin that this page should have
+                var targetOrigin = '*';
+                parent.postMessage(message, targetOrigin);
+
+            }
             self.handleSelectionChanged = function (event) {
                 // Access selected elements via ui.items
-                var selectedProduct = event.detail.value; 
-                console.log("selected product "+selectedProduct);
+                var selectedProduct = event.detail.value;
+                console.log("selected product " + selectedProduct);
+                
+                var productSelectionEvent = {
+                    "eventType": "productSelectionEvent"
+                    , "payload": {
+                        "nameSelectedProduct": selectedProduct
+                    }
+                }
+                self.callParent(productSelectionEvent)
             }
+
+
+            self.init = function () {
+                // listener for events posted on the window;
+                // used for applications running insidean IFRAME to receive events from the
+                // embedding application
+                window.addEventListener("message", function (event) {
+                  console.log("Received message from embedding application " + event);
+                  console.log("Payload =  " + JSON.stringify(event.data));
+                },
+                  false);
+              }
+              $(document).ready(function () { self.init(); })
+
         }
 
         return new CatalogModel();
